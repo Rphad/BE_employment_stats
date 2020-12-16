d678578c4421aa348379a0560bd636c75c8e47e6
Raphaël Deknop <raphael.deknop@ulb.be>
2020-12-16 13:10:10 +0100

    Add files via upload

--- Emstats.r
+++ Emstats.r
@@ -1,27 +1,34 @@
 library(ggplot2)
 require(reshape2)
 
+#The file saved from statbel is loaded
 df <- read.csv("export.csv")
-#mutate(diff_genre = Hommes-Femmes,group_by(region))
-
-
-ggplot(df, aes(x = Year, y= Unemployment.rate, shape=Region, color=Gender)) + geom_point() + geom_line()
 
 mdf <- subset(df, Gender=="Males")
 fdf <- subset(df, Gender=="Females")
 
+#Differences between men and women
 Em_diff = mdf$Employment.rate - fdf$Employment.rate
 Ac_diff = mdf$Activity.rate - fdf$Activity.rate
 Unem_diff= mdf$Unemployment.rate - fdf$Unemployment.rate
 
 diff_df <- data.frame( Region = mdf$Region, Year = mdf$Year, Employment.diff = Em_diff, Activity.diff=Ac_diff, Unemployment.diff = Unem_diff)
 
+#For the plot containing Belgium only
 mdff <- melt(diff_df, id.vars=c("Region","Year"), variable.name="Quantity")
 mdff_global <- subset(mdff, Region=="")
 
+#Unemployment rate in BE & regions
+unem_all <- ggplot(df, aes(x = Year, y= Unemployment.rate, shape=Region, color=Gender)) + geom_point() + geom_line() + ggtitle('Unemployment rate in Belgium')
 
-#ggplot(diff_df, aes(x=Year,shape=Region,)) + geom_point(aes(y=Unemployment.diff)) + geom_point(aes(y=Employment.diff)) + geom_point(aes(y=Activity.diff))  
+#Employment differences across regions
+diff_all <- ggplot(mdff, aes(Year, value, shape=Region)) + geom_point(aes(colour=Quantity)) + geom_line(aes(colour=Quantity)) + ggtitle('Differences in employment related rates across genders and regions in Belgium')
 
-ggplot(mdff, aes(Year, value, shape=Region)) + geom_point(aes(colour=Quantity)) + geom_line(aes(colour=Quantity))
+#Employment diffs in BE only (clearer)
+diff_be <- ggplot(mdff_global, aes(Year, value)) + geom_point(aes(colour=Quantity)) + geom_line(aes(colour=Quantity)) + ggtitle('Differences in employment related rates across genders in Belgium')
 
-ggplot(mdff_global, aes(Year, value)) + geom_point(aes(colour=Quantity)) + geom_line(aes(colour=Quantity))
+
+#Saving
+ggsave(plot=unem_all, "unem_all.pdf")
+ggsave(plot=diff_all, "diff_all.pdf")
+ggsave(plot=diff_be, "diff_be.pdf")
